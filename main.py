779504bb5b874484cad39879c1875f18beedc1d7
제임스 <junwon.lee@majormap.net>
2022-08-19 19:19:45 +0900

    initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import plotly.offline as pyo
+
+
+st.title('채용공고 자연어 분석')
+
+
+@st.cache
+def load_data():
+    data = pd.read_csv('umap_data.csv')
+    return data
+
+
+data_load_state = st.text('Loading data...')
+data = load_data()
+
+
+
+
+pyo.init_notebook_mode()
+
+
+fig = px.scatter(
+    data,
+    x='x',
+    y='y',
+    hover_name='name',
+    width=1000,
+    height=600,
+)
+
+st.write(fig)
